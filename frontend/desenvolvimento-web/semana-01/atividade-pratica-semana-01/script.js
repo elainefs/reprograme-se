3a584d6e8d2a48fa0fbe6febf73b48c535671e5a
Elaine Ferreira <elaineferreira.dev@gmail.com>
2023-11-03 15:37:10 -0300

    Add practices from week 01

---
+++ frontend/desenvolvimento-web/semana-01/atividade-pratica-semana-01/script.js
@@ -0,0 +1,12 @@
+// Selecionar todos elementos da classe 'produto_preco'
+let precos = document.getElementsByClassName('produto_preco');
+
+// Implementar um comando de repetição para analisar o valor 'innerText' de cada um dos elementos e somá-los (lembre-se de converter o valor para float)
+let soma = 0;
+for(let i = 0; i < precos.length; i++){
+  let preco = parseFloat(precos[i].innerText);
+  soma = soma + preco;
+}
+
+// Escrever no conteúdo da página o valor da soma 
+document.write(`Total R$ ${soma}`)
--- frontend/desenvolvimento-web/semana-01/praticas/pratica-01/README.md
+++
@@ -1,2 +0,0 @@
-### Prática - Selecionar elementos do DOM
-Elabore um script, utilizando a linguagem JavaScript, para selecionar e modificar elementos de um arquivo 'index.html'.
